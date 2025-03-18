32fb5ce39c7ac0804608142fdfabf6ff7d856632
GGabrielRodrigues <gabriel.rodrigues2345@discente.ufg.br>
2025-03-17 21:40:03 -0300

    Ultimo exercicio

--- Exercícios/ExerciciosString/src/listastring/Ex07.java
+++ Exercícios/ExerciciosString/src/listastring/Ex07.java
@@ -1,13 +1,26 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package listastring;
 
-/**
- *
- * @author gabri
- */
+import java.util.Scanner;
+
+/*
+Criar um programa que leia 2 valores e mostre a soma, a subtração, a multiplicação e a divisão 
+entre eles. (A+B, A-B, A*B, A/B)
+*/
 public class Ex07 {
-    
+    public static void main(String[] args) {
+        //Leitura dos números
+            Scanner leitor = new Scanner(System.in);
+            System.out.print("Insira o primeiro numero: ");
+            int num1 = leitor.nextInt();
+            System.out.print("Insira o segundo numero: ");
+            int num2 = leitor.nextInt();
+        //
+        
+        //Impressão
+            System.out.println("Soma: " + (num1+num2) +
+                    "\nSubtracao: " + (num1-num2) +
+                    "\nMultiplicacao: " + (num1*num2) + "\nDivisao: " + (num1/num2));
+        //
+    }
 }
